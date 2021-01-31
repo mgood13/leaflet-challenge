3af7bfc2e8c6d12a528f6c79b2249445e2c03f12
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-01-30 21:13:04 -0500

    Adding in the plate lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,44 @@
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-var myMap = L.map("mapid", {
-  center: [37.09, -95.71],
-  zoom: 3.5
-});
+
 
 // Add a tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-}).addTo(myMap);
+});
+
+var plateUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
+d3.json(plateUrl,function(response) {
+    for (var j =0; j<response.features.length; j++){
+        L.geoJSON(response.features[j]
+        ).addTo(myMap)
+    };
+
+    var geoJsonLayer = L.geoJson(response, {
+        onEachFeature: function (feature, layer) {
+            if (layer instanceof L.Polyline) {
+                layer.setStyle({
+                    'color': "red"
+                });
+            }
+        }
+    });
+});
+
+var myMap = L.map("mapid", {
+  center: [37.09, -95.71],
+  zoom: 3.5,
+  layers: [streets]
+});
+
+
+
 
 
 var legend = L.control({ position: "bottomright" });
@@ -36,8 +61,6 @@ legend.addTo(myMap);
 
 
 d3.json(url,function(response) {
-    console.log(response)
-    console.log(response.features.length)
 
     for (var i = 0; i<response.features.length; i++){
 
@@ -91,7 +114,7 @@ d3.json(url,function(response) {
         ).addTo(myMap);
     };
 
-
-
-
 });
+
+
+
